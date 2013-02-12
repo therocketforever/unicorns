7772839180895122e79db01ef4487fd8d431a6ec
DON who is a ghost <therocketforever@me.com>
2013-02-12 15:04:33 -0500

    Uncomment DB EVN for Postgres on Heroku in production & development modes.

--- application.rb
+++ application.rb
@@ -13,7 +13,7 @@ class Application < Sinatra::Base
   end
 
   configure :production do
-    ENV['DATABASE_URL'] || DataMapper.setup(:default, "sqlite://#{Dir.pwd}/development.db") 
+    DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.db") 
   end
 
   helpers do
