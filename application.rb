f0304b16b1ab777753b67ab909abdc01b7f590b9
DON who is a ghost <therocketforever@me.com>
2013-03-01 15:27:50 -0500

    asymetric Google Analytics.

--- application.rb
+++ application.rb
@@ -227,6 +227,7 @@ __END__
   = haml :_header
   = yield
   = haml :_footer
+  = haml :_analytics
 
 @@_header
 %header
@@ -286,3 +287,10 @@ __END__
 
 @@_tags
 -#%p I am @@_tags!!
+
+@@_analytics
+%script
+  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
