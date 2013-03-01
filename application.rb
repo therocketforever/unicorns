bbb251cf283d80f14f5cc7fa3208d55b525b18d3
DON who is a ghost <therocketforever@me.com>
2013-03-01 16:00:00 -0500

    Disabled tracking code, I think the HAML is wrong

--- application.rb
+++ application.rb
@@ -227,7 +227,7 @@ __END__
   = haml :_header
   = yield
   = haml :_footer
-  = haml :_analytics
+  -#= haml :_analytics
 
 @@_header
 %header
@@ -288,9 +288,9 @@ __END__
 @@_tags
 -#%p I am @@_tags!!
 
-@@_analytics
-%script
-  var _gaq=[['_setAccount','UA-38958517-1'],['_trackPageview']];
-  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
+-#@@_analytics
+-#%script
+-#  var _gaq=[['_setAccount','UA-38958517-1'],['_trackPageview']];
+-#  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+-#    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+-#    s.parentNode.insertBefore(g,s)}(document,'script'));
